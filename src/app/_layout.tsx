2d2121da2ce9168c78119c31d3b98f2d718cfcb5
Cesar Silva da Conceição Filho <cesar@Cesars-Laptop.local>
2023-10-23 18:10:50 -0300

    feat: Add useEffects in _layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,46 @@
-import { Stack } from "expo-router";
 import '@tamagui/core/reset.css'
 
+import React from "react";
+import { Stack } from "expo-router";
 import { TamaguiProvider } from 'tamagui'
 
+import { AppState, useColorScheme } from "react-native";
+
 import config from '../tamagui.config'
+import { useFonts } from "expo-font";
 
 export default function Layout() {
+    const appState = React.useRef(AppState.currentState);
+    const [appStateVisible, setAppStateVisible] = React.useState(appState.current);
+    const colorScheme = useColorScheme();
+    const [activeColorScheme, setActiveColorScheme] = React.useState(colorScheme);
+
+    React.useEffect(() => {
+        if (appStateVisible === "active") {
+          setActiveColorScheme(colorScheme);
+        }
+      }, [appStateVisible, colorScheme]);
+    
+    React.useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+        appState.current = nextAppState;
+        setAppStateVisible(appState.current);
+    });
+
+    return () => {
+        subscription.remove();
+    };
+    }, []);
+    
+    const [loaded] = useFonts({
+        Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+        InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+      });
+    
+    if (!loaded) {
+    return null;
+    }
+
     return (
         <TamaguiProvider config={config}>
             <Stack>
@@ -16,7 +51,7 @@ export default function Layout() {
                     headerShown: false,
                 }}
                 />
-        </Stack>
-            </TamaguiProvider>
+            </Stack>
+        </TamaguiProvider>
     )
 }
